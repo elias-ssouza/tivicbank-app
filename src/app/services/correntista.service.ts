f65461c3ca83e4276a25ccd14b2f3143d4e8af04
elias-ssouza <elias.ssouza@hotmail.com>
2023-04-10 19:09:28 -0300

    pequenas alterações (espaçamento)

--- src/app/services/correntista.service.ts
+++ src/app/services/correntista.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 const baseUrl = 'http://localhost:8080';
+
 @Injectable({
   providedIn: 'root'
 })
